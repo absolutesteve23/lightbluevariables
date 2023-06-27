30624cd75cb07e8a8e71df2f1aa90db3baceaee4
absolutesteve23 <137877983+absolutesteve23@users.noreply.github.com>
2023-06-27 18:39:06 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+(function () {
+    const plugin = BF2042Portal.Plugins.getPlugin("red-variables");
+
+    plugin.initializeWorkspace = function () {
+        const theme = _Blockly.getMainWorkspace().getTheme();
+
+        const variableStyle = theme.blockStyles["variable-block-style"];
+        variableStyle.colourPrimary = "#ADD8E6"; // Body of Subroutine R - 110 #640000
+        variableStyle.colourSecondary = "#ADD8E6"; //
+        variableStyle.colourTertiary = "##ADD8E6"; // Edge of Subroutine R - 70
+
+        _Blockly.getMainWorkspace().setTheme(theme);
+    };
+})();
