a94bf6358a9ec01133126b13ddb8b8309738d134
absolutesteve23 <137877983+absolutesteve23@users.noreply.github.com>
2023-06-27 19:08:50 +1000

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,9 @@
         const theme = _Blockly.getMainWorkspace().getTheme();
 
         const variableStyle = theme.blockStyles["variable-block-style"];
-        variableStyle.colourPrimary = "#000000"; // Body of Subroutine R - 110 #000000
-        variableStyle.colourSecondary = "#000000"; //
-        variableStyle.colourTertiary = "#000000"; // Edge of Subroutine R - 70
+        variableStyle.colourPrimary = "#460000"; // Body of Subroutine R - 110 #460000
+        variableStyle.colourSecondary = "#460000"; //
+        variableStyle.colourTertiary = "#460000"; // Edge of Subroutine R - 70
 
         _Blockly.getMainWorkspace().setTheme(theme);
     };
